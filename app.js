4e0044d7c91325b714bd68192da57cd7bf7e2be6
esteban <lic.olmedoesteban@gmail.com>
2023-06-21 19:50:52 -0300

    TP5

--- app.js
+++ app.js
@@ -4,19 +4,23 @@ const PORT = 3000;
 
 app.get("/index", (req, res) => {
     res.send("pagina principal")
-})
+});
+
 app.get("/cart", (req, res) => {
     res.send("carrito")
-})
+});
+
 app.get("/detalle", (req, res) => {
     res.send("detalle de producto")
-})
+});
+
 app.get("/login", (req, res) => {
     res.send("iniciar sesion")
-})
+});
+
 app.get("/register", (req, res) => {
     res.send("registrarse")
-})
+});
 
 
 
